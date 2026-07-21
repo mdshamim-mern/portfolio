00537b053c314bd9d339f2a24638b6783e3001ce
Md Shamim <mdshamim.mern@gmail.com>
2026-07-21 22:24:01 +0600

    Force clear all cookies and storage on logout

--- src/components/admin/Sidebar.tsx
+++ src/components/admin/Sidebar.tsx
@@ -15,11 +15,15 @@ export default function Sidebar() {
   ];
 
   const handleLogout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("admin_token");
-    document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-    document.cookie = "admin_token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-    window.location.href = "/login";
+    localStorage.clear();
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+    window.location.replace("/login");
   };
 
   return (
